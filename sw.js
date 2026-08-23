eba4d6a2578686b6c3371a653f9fd774be6dfc52
alejandrocaporali98-cell <alejandro.caporali98@outlook.com>
2026-08-23 14:53:04 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Spindle service worker
    Sube la versión cada vez que cambies index.html; así el navegador
    descarta la caché vieja en lugar de servir la versión anterior. */
-const VERSION = 'spindle-v11';
+const VERSION = 'spindle-v13';
 
 const SHELL = [
   './',
