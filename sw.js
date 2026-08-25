da6ea72e0d85e8e25dbcfdcc1c63917f4687d983
alejandrocaporali98-cell <alejandro.caporali98@outlook.com>
2026-08-24 18:20:55 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* M29 service worker
    Sube la versión cada vez que cambies index.html; así el navegador
    descarta la caché vieja en lugar de servir la versión anterior. */
-const VERSION = 'm29-v2';
+const VERSION = 'm29-v3';
 
 const SHELL = [
   './',
